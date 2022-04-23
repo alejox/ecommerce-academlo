23cad21978ca8b15d92a5402986150cd09a73999
alejox <alejoxdrums@gmail.com>
2022-04-22 23:49:54 -0500

    ACT

--- js/main.js
+++ js/main.js
@@ -69,6 +69,33 @@ function cartMenu () {
 cartMenu();
 
 /*---------------------Products------------------------------------*/
+document.addEventListener('DOMContentLoaded', function () {
+  mixitup('.products__content', {
+    selectors: {
+      target: '.products__card'
+    },
+    animation: {
+      duration: 300
+    }
+  }).filter('all')
+})
+
+function activeProduct () {
+  const linksProducts = document.querySelectorAll('.products__item')
+
+  for (let i = 0; i < linksProducts.length; i++) {
+    linksProducts[i].addEventListener('click', function () {
+      for (let j = 0; j < linksProducts.length; j++) {
+        linksProducts[j].classList.remove('active-product')
+      }
+      this.classList.add('active-product')
+    })
+  }
+}
+
+activeProduct();
+
+
 const items = [
   {
     id: 1,
@@ -95,79 +122,6 @@ const items = [
     quantity: 20
   }
 ]
-
-const db = {
-  items: window.localStorage.getItem('products') ? JSON.parse(window.localStorage.getItem('products')) : items,
-  methods: {
-    find: (id) => {
-      return db.items.find(item => item.id === id)
-    },
-    getAll: () => {
-      return db.items
-    },
-    remove: (items) => {
-      items.forEach(item => {
-        const product = db.methods.find(item.id)
-        product.quantity = product.quantity - item.quantity
-      })
-    }
-  }
-}
-
-
-  const renderProducts = () => {
-  const productsContainer = document.querySelector('#products .products__content')
-  const products = db.methods.getAll()
-  let html = ''
-
-  products.forEach(product => {
-    html += `
-      <article class="products__card ${product.category}">
-      <div class="products__shape">
-        <img src="${product.image}" alt="${product.name}" class="products__img">
-      </div>
-
-      <div class="products__data">
-        <h2 class="products__price">${numberToCurrency(product.price)} <span class="products__quantity">| Stock: ${product.quantity}</span></h2>
-        <h3 class="products__name">${product.name}</h3>
-
-        <button class="button products__button" data-id="${product.id}">
-          <i class='bx bx-plus'></i>
-        </button>
-      </div>
-      </article>`
-  })
-
-  productsContainer.innerHTML += html
-
-  const productsButton = document.querySelectorAll('.products__button')
-
-  productsButton.forEach(button => {
-    button.addEventListener('click', () => {
-      const id = parseInt(button.getAttribute('data-id'))
-      const product = db.methods.find(id)
-
-      if (product && product.quantity > 0) {
-        cart.methods.add(id, 1)
-        renderCart()
-      } else {
-        window.alert('Sorry, we are out of stock')
-      }
-    })
-  })
-
-  window.localStorage.setItem('products', JSON.stringify(db.items))
-}
-
-
-function numberToCurrency (value) {
-  return new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD'
-  }).format(value)
-}
-
-numberToCurrency();
 
 const cart = {
   items: window.localStorage.getItem('cart') ? JSON.parse(window.localStorage.getItem('cart')) : [],
@@ -330,4 +284,79 @@ const cart = {
 }
 
 renderCart();
+
+function numberToCurrency (value) {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+  }).format(value)
+}
+
+numberToCurrency();
+
+
+const db = {
+  items: window.localStorage.getItem('products') ? JSON.parse(window.localStorage.getItem('products')) : items,
+  methods: {
+    find: (id) => {
+      return db.items.find(item => item.id === id)
+    },
+    getAll: () => {
+      return db.items
+    },
+    remove: (items) => {
+      items.forEach(item => {
+        const product = db.methods.find(item.id)
+        product.quantity = product.quantity - item.quantity
+      })
+    }
+  }
+}
+
+
+  const renderProducts = () => {
+  const productsContainer = document.querySelector('#products .products__content')
+  const products = db.methods.getAll()
+  let html = ''
+
+  products.forEach(product => {
+    html += `
+      <article class="products__card ${product.category}">
+      <div class="products__shape">
+        <img src="${product.image}" alt="${product.name}" class="products__img">
+      </div>
+
+      <div class="products__data">
+        <h2 class="products__price">${numberToCurrency(product.price)} <span class="products__quantity">| Stock: ${product.quantity}</span></h2>
+        <h3 class="products__name">${product.name}</h3>
+
+        <button class="button products__button" data-id="${product.id}">
+          <i class='bx bx-plus'></i>
+        </button>
+      </div>
+      </article>`
+  })
+
+  productsContainer.innerHTML += html
+
+  const productsButton = document.querySelectorAll('.products__button')
+
+  productsButton.forEach(button => {
+    button.addEventListener('click', () => {
+      const id = parseInt(button.getAttribute('data-id'))
+      const product = db.methods.find(id)
+
+      if (product && product.quantity > 0) {
+        cart.methods.add(id, 1)
+        renderCart()
+      } else {
+        window.alert('Sorry, we are out of stock')
+      }
+    })
+  })
+
+  window.localStorage.setItem('products', JSON.stringify(db.items))
+}
+
+
 
